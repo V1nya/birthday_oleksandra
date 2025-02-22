bfecfad1f96579261e25226d58f5245f997112ae
k.pysanka <k.pysanka@post.mil.gov.ua>
2025-02-22 20:08:10 +0200

    update

--- src/Gift/GiftScene.jsx
+++ src/Gift/GiftScene.jsx
@@ -2,9 +2,16 @@ import { useState, useEffect } from "react";
 import "./GiftScene.css";
 
 const images = [
-    { path: "/img/f6.jpeg", text: "Бажаємо що б ти була прикладом для інших" },
+    { path: "/img/f06.jpeg", text: "Вітаємо з свʼятом і хочемо побажати" },
+    { path: "/img/f05.jpeg", text: "Щоб кожна проблему зустрічала з посмішкою" },
+    // { path: "/img/f05.jpeg", text: "Щоб кожна проблему зустрічала з посмішкою" },
+    { path: "/img/f04.jpeg", text: "Кожен ранок був чудовий" },
+    { path: "/img/f01.jpeg", text: "Легко вчилась сама" },
+    { path: "/img/f6.jpeg", text: "І прикладом для інших" },
+    { path: "/img/f02.jpeg", text: "Завжди досягала своїх цілей" },
     { path: "/img/f2.jpeg", text: "Мала гарний аппетит " },
     { path: "/img/f3.jpeg", text: "Завжди була в гарній компанії " },
+    { path: "/img/f03.jpeg", text: "Чудово виглядала" },
     { path: "/img/f5.jpeg", text: "І мала найкращю сімʼю" },
 ];
 
